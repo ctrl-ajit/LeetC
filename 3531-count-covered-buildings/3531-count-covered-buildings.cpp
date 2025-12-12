b2515bf8b744c08b8c55a0644ce0953393109fe1
Ajit Kumar <ajit191121@gmail.com>
2025-12-12 11:08:53 +0530

    Time: 949 ms (19.48%), Space: 445.6 MB (24.74%) - LeetHub

--- 3531-count-covered-buildings/3531-count-covered-buildings.cpp
+++ 3531-count-covered-buildings/3531-count-covered-buildings.cpp
@@ -1,33 +1,32 @@
-const int SZ=100001;
-int xMin[SZ], xMax[SZ], yMin[SZ], yMax[SZ];
 class Solution {
 public:
-    int countCoveredBuildings(int n, vector<vector<int>>& buildings) {
-        int M=0, N=0;
-        for(auto& B: buildings){
-            const int x=B[0], y=B[1];
-            M=max(x, M);
-            N=max(y, N);
-        }
-        memset(xMax, 0, sizeof(int)*(N+1));
-        memset(yMax, 0, sizeof(int)*(M+1));
-        fill(xMin, xMin+(N+1), INT_MAX);
-        fill(yMin, yMin+(M+1), INT_MAX);
+    int countCoveredBuildings(int k, vector<vector<int>>& b) {
+        int n=b.size();
 
-        for(auto& B: buildings){
-            const int x=B[0], y=B[1];
-            xMin[y]=min(xMin[y], x);
-            xMax[y]=max(xMax[y], x);
-            yMin[x]=min(yMin[x], y);
-            yMax[x]=max(yMax[x], y);
+        unordered_map<int,vector<int>>mpx;
+        unordered_map<int,vector<int>>mpy;
+        set<pair<int,int>>s;
+
+        for(int i=0;i<n;i++){
+            mpx[b[i][0]].push_back(b[i][1]);
+            mpy[b[i][1]].push_back(b[i][0]);
         }
-        int cnt=0;
-        for(auto& B: buildings){
-            const int x=B[0], y=B[1];
-            const bool coverX=(xMin[y]<x && x<xMax[y]);
-            const bool coverY=(yMin[x]<y && y<yMax[x]);
-            cnt+=(coverX && coverY);
+
+        for(auto &ele:mpx){
+            int x=ele.first;
+            vector<int>v=ele.second;
+            sort(v.begin(),v.end());
+            s.insert({x,v[0]});
+            s.insert({x,v.back()});
         }
-        return cnt;
+        for(auto &ele:mpy){
+            int y=ele.first;
+            vector<int>v=ele.second;
+            sort(v.begin(),v.end());
+            s.insert({v[0],y});
+            s.insert({v.back(),y});
+        }
+        return n-s.size();
+
     }
 };
